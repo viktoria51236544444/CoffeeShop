ad707441e1428ae3e9c9e631c41288f3af6fe4dd
Aidana190 <aidanabekboeva5@gmail.com>
2024-02-20 22:01:35 +0600

    recommended finish

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-
 import React from "react";
 import MainRoutes from "./routes/MainRoutes";
 import ReserveTable from "./homepage/ReserveTable";
 import Recomended from "./homepage/Recomended";
+import OurBlog from "./homepage/OurBlog";
 
 const App = () => {
-  return <div>
-    <ReserveTable/>
-    <Recomended/>
-    <MainRoutes/>
-  </div>;
+  return (
+    <div>
+      <OurBlog />
+      <ReserveTable />
+      <Recomended />
+      <MainRoutes />
+    </div>
+  );
 };
 
-
-export default App
+export default App;
--- src/homepage/ReserveTable.jsx
+++ src/homepage/ReserveTable.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import reserve from './assets/Reserve Img.png'
-import './aidana.scss'
+import React from "react";
+import reserve from "./assets/Reserve Img.png";
+import "./aidana.scss";
+import beans from "./assets/reserve-img/form-beanRight.png";
+import bg2 from "./assets/reserve-img/reserver-gradient2.png";
+import bg from "./assets/reserve-img/reserve-gradient1.png";
 
 const ReserveTable = () => {
   return (
-    <form className='reserve'>
-      <div className='reserve_table'>
-      <div className='img_reserve'>
-        <img src={reserve} width={400} alt="" />
+    <form className="reserve">
+      <div className="reserve_table">
+        <div className="img_reserve">
+          <img src={reserve} width={400} alt="" />
+        </div>
+        <div className="inputs_reserve">
+          <input className="input_input" type="text" placeholder="Date" />
+          <div>
+            <input
+              className="input_input_time"
+              type="text"
+              placeholder="Time"
+            />
+            <input type="text" placeholder="Guest" />
+          </div>
+          <input className="input_input" type="text" placeholder="Name" />
+          <input
+            className="input_input"
+            type="text"
+            placeholder="Phone number"
+          />
+          <div>
+            <input
+              className="input_input_email"
+              type="email"
+              placeholder="Your Email"
+            />
+            <button className="button_reserve">Reserve</button>
+          </div>
+        </div>
       </div>
-      <div className='inputs_reserve'>
-         <input className='input_input' type="text" placeholder='Date'/>
-         <div>
-         <input className='input_input_time' type="text" placeholder='Time'/>
-         <input type="text" placeholder='Guest' />
-         </div>
-         <input className='input_input' type="text" placeholder='Name'/>
-         <input className='input_input' type="text" placeholder='Phone number' />
-         <div>
-         <input className='input_input_email' type="email" placeholder='Your Email'/>
-         <button className='button_reserve'>Reserve</button>
-         </div>
-      </div>
-    </div>
     </form>
-  )
-}
+  );
+};
 
-export default ReserveTable
+export default ReserveTable;
