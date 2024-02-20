663403fe8fa2e139f5dc2c423f83f0cf60bb4753
Oxana <yurchenko.o@gmail.com>
2024-02-20 15:09:56 +0600

    fixed review-sec

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-
 import React from "react";
 import MainRoutes from "./routes/MainRoutes";
 import ReserveTable from "./homepage/ReserveTable";
 
 const App = () => {
-  return <div>
-    <ReserveTable/>
-    <MainRoutes/>
-  </div>;
+  return (
+    <div>
+      <ReserveTable />
+      <MainRoutes />
+    </div>
+  );
 };
 
-
-export default App
+export default App;
