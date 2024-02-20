e5bd49b335dac6ad3107c5ea208824e5bd5573e3
Aidana190 <aidanabekboeva5@gmail.com>
2024-02-20 14:53:15 +0600

    recommended finish

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React from "react";
 import MainRoutes from "./routes/MainRoutes";
 import ReserveTable from "./homepage/ReserveTable";
+import Recomended from "./homepage/Recomended";
 
 const App = () => {
   return <div>
     <ReserveTable/>
+    <Recomended/>
     <MainRoutes/>
   </div>;
 };
