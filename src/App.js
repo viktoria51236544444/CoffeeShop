3ada98b5a074bbff8f71f3aa616f47606fceb157
Oxana <yurchenko.o@gmail.com>
2024-02-21 16:27:06 +0600

    added reviewsection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
-import Navbar from "./homepage/Navbar";
-import Header from "./homepage/Header";
+// import Navbar from "./homepage/Navbar";
+// import Header from "./homepage/Header";
+import ReserveTable from "./homepage/ReserveTable";
+import Recomended from "./homepage/Recomended";
+import ReviewSection from "./homepage/ReviewSection";
+import MainRoutes from "./routes/MainRoutes";
 
 const App = () => {
   return (
@@ -8,6 +12,7 @@ const App = () => {
       <ReserveTable />
 
       <Recomended />
+      <ReviewSection />
 
       <MainRoutes />
     </div>
