1f6f00cf368669313c28f896cc3de23aa263c68d
Your Name <you@example.com>
2024-02-21 10:45:24 +0600

    viktoria

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import MainRoutes from "./routes/MainRoutes";
 import ReserveTable from "./homepage/ReserveTable";
 import Recomended from "./homepage/Recomended";
+import Navbar from "./homepage/Navbar";
+import Header from "./homepage/Header";
+import MainContentBlock1 from "./homepage/MainContentBlock1";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
+      <Header />
+      <MainContentBlock1 />
       <ReserveTable />
       <Recomended />
       <MainRoutes />
