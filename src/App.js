18041370fa66ee9b5c71dbde5fa9d0326e68654e
Oxana <yurchenko.o@gmail.com>
2024-02-22 01:06:43 +0600

    added subscribe and footer sections

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import ReserveTable from "./homepage/ReserveTable";
 import Recomended from "./homepage/Recomended";
 import ReviewSection from "./homepage/ReviewSection";
 import MainRoutes from "./routes/MainRoutes";
+import Subscribe from "./homepage/Subscribe";
+import OurBlog from "./homepage/OurBlog";
+import Footer from "./homepage/Footer";
 
 const App = () => {
   return (
     <div>
+      <OurBlog />
       <ReserveTable />
 
       <Recomended />
       <ReviewSection />
+      <Subscribe />
+      <Footer />
 
       <MainRoutes />
     </div>
