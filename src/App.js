28f271913d00e86c4f2fe498b3c29ea858e7c0c5
Aidana190 <aidanabekboeva5@gmail.com>
2024-02-23 16:39:56 +0600

    fixing search finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import MainRoutes from "./routes/MainRoutes";
+import Navbar from "./homepage/Navbar";
+import Recomended from "./homepage/Recomended";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
 
       <MainRoutes />
     </div>
