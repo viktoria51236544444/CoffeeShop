48548f3d8fed32eec77e44b32f1c796522589ab2
Oxana <yurchenko.o@gmail.com>
2024-02-23 16:48:14 +0600

    check styles

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -11,16 +11,16 @@ import Homepage from "../pages/Homepage";
 
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
-    { id: 1, link: "/edit/:id", element: <EditPage /> },
-    { id: 2, link: "/admin", element: <AdminPage /> },
-    { id: 3, link: "/products", element: <ProductPage /> },
-    { id: 4, link: "/menu", element: <Menu /> },
-    { id: 5, link: "/cart", element: <Cart /> },
-    { id: 6, link: "/payment", element: <PaymentForm /> },
-    { id: 7, link: "/comments", element: <Comments /> },
-    // { id: 8, link: "/login", element: <Comments /> },
-    // { id: 9, link: "/register", element: <Comments /> },
-    { id: 10, link: "/", element: <Homepage /> },
+    { id: 1, link: "/", element: <Homepage /> },
+    { id: 2, link: "/edit/:id", element: <EditPage /> },
+    { id: 3, link: "/admin", element: <AdminPage /> },
+    { id: 4, link: "/products", element: <ProductPage /> },
+    { id: 5, link: "/menu", element: <Menu /> },
+    { id: 6, link: "/cart", element: <Cart /> },
+    { id: 7, link: "/payment", element: <PaymentForm /> },
+    { id: 8, link: "/comments", element: <Comments /> },
+    // { id: 9, link: "/login", element: <Comments /> },
+    // { id: 10, link: "/register", element: <Comments /> },
   ];
 
   return (
